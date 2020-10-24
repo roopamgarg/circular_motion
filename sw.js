2401569e45c00b74c9ce19a29cf8df78211861eb
roopamgarg <roopamg777@gmail.com>
2020-10-24 17:04:15 +0530

    initail commits

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
 const CACHE_STATIC_NAME = "static-v2";
-
+const ROOT_URL = "/circular_motion/"
 const STATIC_FILES = [
-    "./",
-    "./index.html",
-    "./src/js/external/perlin.js",
-    "./src/js/app.js",
-    "./src/js/cursor.js",
-    "./src/js/debounce.js",
-    "./src/css/style.css",
-    "./src/assets/videos/about.mp4",
-    "./src/assets/images/astro.jpg",
-    "./src/assets/images/hellowean.jpg",
-    "./src/assets/images/horror.jpg",
-    "./src/assets/images/movie.jpg",
-    "https://fonts.googleapis.com/css2?family=Cinzel:wght@400;500;600;700;800;900&display=swap",
+    `${ROOT_URL}`,
+    `${ROOT_URL}index.html`,
+    `${ROOT_URL}src/js/external/perlin.js`,
+    `${ROOT_URL}src/js/app.js`,
+    `${ROOT_URL}src/js/cursor.js`,
+    `${ROOT_URL}src/js/debounce.js`,
+    `${ROOT_URL}src/css/style.css`,
+    `${ROOT_URL}src/assets/videos/about.mp4`,
+    `${ROOT_URL}src/assets/images/astro.jpg`,
+    `${ROOT_URL}src/assets/images/hellowean.jpg`,
+    `${ROOT_URL}src/assets/images/horror.jpg`,
+    `${ROOT_URL}src/assets/images/movie.jpg`,
+    `https://fonts.googleapis.com/css2?family=Cinzel:wght@400;500;600;700;800;900&display=swap`,
 ]
 
 self.addEventListener("install",(event) => {
