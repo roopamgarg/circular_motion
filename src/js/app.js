734843cdb9d406a08afc50dfca27c558b0da47fd
roopamgarg <roopamg777@gmail.com>
2020-10-24 17:05:22 +0530

    initail commits

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ const len = elements.length;
 
 if('serviceWorker' in navigator){
   navigator.serviceWorker
-  .register("/sw.js")
+  .register("./sw.js")
   .then(function () {
     console.log('Service worker registered!');
   })
