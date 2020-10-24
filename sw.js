bb05e0ff18531cbf9a23d9b7e9b3895ee1a37eca
roopamgarg <roopamg777@gmail.com>
2020-10-24 22:55:50 +0530

    added dynamic caching

--- sw.js
+++ sw.js
@@ -1,34 +1,47 @@
 const CACHE_STATIC_NAME = "static-v2";
-const ROOT_URL = "/circular_motion/"
+const CACHE_DYNAMIC_NAME = "dynamic-v2";
+
+const ROOT_URL = "/circular_motion/";
 const STATIC_FILES = [
-    `${ROOT_URL}`,
-    `${ROOT_URL}index.html`,
-    `${ROOT_URL}src/js/external/perlin.js`,
-    `${ROOT_URL}src/js/app.js`,
-    `${ROOT_URL}src/js/cursor.js`,
-    `${ROOT_URL}src/js/debounce.js`,
-    `${ROOT_URL}src/css/style.css`,
-    `${ROOT_URL}src/assets/videos/about.mp4`,
-    `${ROOT_URL}src/assets/images/astro.jpeg`,
-    `${ROOT_URL}src/assets/images/hellowean.jpeg`,
-    `${ROOT_URL}src/assets/images/horror.jpeg`,
-    `${ROOT_URL}src/assets/images/movie.jpeg`,
-    `https://fonts.googleapis.com/css2?family=Cinzel:wght@400;500;600;700;800;900&display=swap`,
-]
+  `${ROOT_URL}`,
+  `${ROOT_URL}index.html`,
+  `${ROOT_URL}src/js/external/perlin.js`,
+  `${ROOT_URL}src/js/app.js`,
+  `${ROOT_URL}src/js/cursor.js`,
+  `${ROOT_URL}src/js/debounce.js`,
+  `${ROOT_URL}src/css/style.css`,
+  `${ROOT_URL}src/assets/videos/about.mp4`,
+  `${ROOT_URL}src/assets/images/astro.jpeg`,
+  `${ROOT_URL}src/assets/images/hellowean.jpeg`,
+  `${ROOT_URL}src/assets/images/horror.jpeg`,
+  `${ROOT_URL}src/assets/images/movie.jpeg`,
+  `https://fonts.googleapis.com/css2?family=Cinzel:wght@400;500;600;700;800;900&display=swap`,
+];
 
-self.addEventListener("install",function(event) {
-    caches.open();
-    event.waitUntil(
-        caches.open(CACHE_STATIC_NAME).then((cache) => {
-           return cache.addAll(STATIC_FILES);
+self.addEventListener("install", function (event) {
+  //  caches.open();
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME).then((cache) => {
+      return cache.addAll(STATIC_FILES);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  return self.ClientRectList.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).catch(() => {
+      fetch(event.request)
+        .then((res) => {
+          return caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
+            cache.put(event.request.url, res.clone());
+            return res;
+          });
         })
-    )
-})
-
-self.addEventListener("activate", event => {
-    return self.ClientRectList.claim();
-})
-
-self.addEventListener("fetch", event => {
-    event.respondWith(caches.match(event.request))
-})
+        .catch((err) => {});
+    })
+  );
+});
